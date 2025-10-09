f148600724ab7970ad1ea951b79937ce523ada3e
nanymota <helainegmota.1204@gmail.com>
2025-10-09 16:36:24 -0300

    Atualização - lavis

--- app/controllers/bannerController.js
+++ app/controllers/bannerController.js
@@ -17,30 +17,45 @@ const bannerController = {
 
     atualizarBanners: async (req, res) => {
         try {
+            console.log('Arquivos recebidos:', req.files);
+            
             if (req.files) {
                 // Processar banners desktop
-                if (req.files.banner_desk_1) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_desk_1[0].filename, 'Home', 1, 1);
+                if (req.files.banner_desk_1 && req.files.banner_desk_1[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_desk_1[0].filename;
+                    console.log('Atualizando banner desktop 1:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 1, 1);
                 }
-                if (req.files.banner_desk_2) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_desk_2[0].filename, 'Home', 2, 1);
+                if (req.files.banner_desk_2 && req.files.banner_desk_2[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_desk_2[0].filename;
+                    console.log('Atualizando banner desktop 2:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 2, 1);
                 }
-                if (req.files.banner_desk_3) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_desk_3[0].filename, 'Home', 3, 1);
+                if (req.files.banner_desk_3 && req.files.banner_desk_3[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_desk_3[0].filename;
+                    console.log('Atualizando banner desktop 3:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 3, 1);
                 }
 
                 // Processar banners mobile
-                if (req.files.banner_mobile_1) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_mobile_1[0].filename, 'Home', 4, 1);
+                if (req.files.banner_mobile_1 && req.files.banner_mobile_1[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_mobile_1[0].filename;
+                    console.log('Atualizando banner mobile 1:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 4, 1);
                 }
-                if (req.files.banner_mobile_2) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_mobile_2[0].filename, 'Home', 5, 1);
+                if (req.files.banner_mobile_2 && req.files.banner_mobile_2[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_mobile_2[0].filename;
+                    console.log('Atualizando banner mobile 2:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 5, 1);
                 }
-                if (req.files.banner_mobile_3) {
-                    await bannerModel.updateOrCreate('imagem/banners/' + req.files.banner_mobile_3[0].filename, 'Home', 6, 1);
+                if (req.files.banner_mobile_3 && req.files.banner_mobile_3[0]) {
+                    const filePath = 'imagem/banners/' + req.files.banner_mobile_3[0].filename;
+                    console.log('Atualizando banner mobile 3:', filePath);
+                    await bannerModel.updateOrCreate(filePath, 'Home', 6, 1);
                 }
             }
 
+            console.log('Banners atualizados com sucesso');
             res.redirect('/homeadm?sucesso=banners_atualizados');
         } catch (error) {
             console.log('Erro ao atualizar banners:', error);
